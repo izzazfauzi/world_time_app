e2f7cb67889c8c3b4db517dd85c73c4d7dbb0b56
izzazfauzi <m.izzaz93@gmail.com>
2020-07-29 19:32:21 +0800

    World Time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,17 +8,26 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
+    // make the request
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
+        await get('http://worldtimeapi.org/api/timezone/Asia/Kuala_Lumpur');
     Map data = jsonDecode(response.body);
-    print(data['title']);
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    //print('$datetime - $offset');
+
+    // create DateTime console
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
